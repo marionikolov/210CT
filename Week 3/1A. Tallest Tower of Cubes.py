1ae0904faf30259173f704ff3db1f1916dd40ed0
Mario Nikolov <marionikolov1@gmail.com>
2016-11-01 12:56:34 +0000

    [w3/1a] implemented case where lower cube needs to be changed

--- Week 3/1A. Tallest Tower of Cubes.py
+++ Week 3/1A. Tallest Tower of Cubes.py
@@ -1,12 +1,10 @@
 def findCubeDiffColour(listOfCubes, length, colour):
     for item in listOfCubes:
-        if item[0] == length:
-            return item
-        else if item[1] != colour:
+        if item[0] == length and item[1] != colour:
             return item
 
 # cube format: (7, "blue")
-def CubeTower(inputCubes, towerOfCubes = [], discardedCubes = [], duplicates = True, isSorted = False):
+def buildCubeTower(inputCubes, towerOfCubes = [], discardedCubes = [], duplicates = True, isSorted = False):
         if len(inputCubes) == 0:
             print(str(towerOfCubes))
             return () # is this required?
@@ -18,15 +16,27 @@ def CubeTower(inputCubes, towerOfCubes = [], discardedCubes = [], duplicates = T
         penultCube = inputCube[-2]
         if ultCube[1] == penultCube[1]: # the last two cubes are of the same colour
             currentCubeSameSizeDiffColour = findCubeDiffColour(inputCubes, towerOfCubes[-1][0], towerOfCubes[-1][1])
-            if currentCubeSameSizeDiffColour is not None:
+            lowerCubeSameSizeDiffColour = findCubeDiffColour(inputCubes, towerOfCubes[-2][0], towerOfCubes[-2][1])
+            if currentCubeSameSizeDiffColour is not None: # if there is a cube of the same size but of different colour to substitute the topmost coinciding one
                 discardedCubes.append(towerOfCubes[-1]) # put current topmost cube in discarded
                 del towerOfCubes[-1] # remove current topmost from tower
                 towerOfCubes.append(currentCubeSameSizeDiffColour)
                 inputCubes.remove(currentCubeSameSizeDiffColour) # remove currentCubeSameSizeDiffColour from inputCubes
-                CubeTower(inputCubes, towerOfCubes, discardedCubes, True, False)
+                buildCubeTower(inputCubes, towerOfCubes, discardedCubes, False, True) # check the last two parameters
 
-            lowerCubeSameSizeDiffColour = #
-            elif lowerCubeSameSizeDiffColour is not None: # there is a bigger cube of same size but diff colour
+
+            elif lowerCubeSameSizeDiffColour is not None: # if there is a bigger cube of same size but diff colour to substitute the lower coinciding cube
+                currentCubeSize = towerOfCubes[-1][0] # get the size of the current topmost cube
+                inputCubes.append(towerOfCubes[-1]) # return the current topmost cube to the inputCubes list
+                del towerOfCubes[-1]
+                for cube in discardedCubes: # return all cubes of the same size as the now returned topmost cube to the inputCubes list
+                    if cube[0] == currentCubeSize:
+                        inputCubes.append(cube)
+                discardedCubes.append(towerOfCubes[-1])
+                del towerOfCubes[-1]
+                towerOfCubes.append(lowerCubeSameSizeDiffColour)
+                inputCubes.remove(currentCubeSameSizeDiffColour)
+                buildCubeTower(inputCubes, towerOfCubes, discardedCubes, True, False) # check the last two parameters
 
             else:
                 pass
