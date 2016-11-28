2cb2b82bd01aaa7ca5f4ea7effe810b3c41d4457
Mario Nikolov <marionikolov1@gmail.com>
2016-11-28 00:07:12 +0000

    [w4/1] added coded task; prepared filed for pseudocode

---
+++ Week 4/1. Binary Search for a Number in a Range.py
@@ -0,0 +1,28 @@
+def isSorted(lst):
+    for i in range(len(lst)-2):
+        if lst[i] > lst[i+1]:
+            return False
+    return True
+
+def binarySearchWithinRange(lst, x, y):
+    if isSorted(lst) != True:
+        lst.sort()
+    firstLoc = 0
+    lastLoc = len(lst) - 1
+    while lastLoc >= firstLoc:
+        testLoc = firstLoc + (lastLoc - firstLoc)//2
+        print(firstLoc)
+        print(lastLoc)
+        print(testLoc)
+        print("---")
+        if lst[testLoc] >= x and lst[testLoc] <= y:
+            return True
+        elif lst[testLoc] < x:
+            firstLoc = testLoc + 1
+        else:
+            lastLoc = testLoc - 1
+    return False
+
+a = [2,3,4,7,9,13]
+result = binarySearchWithinRange(a, 10, 14)
+print(result)
