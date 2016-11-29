06574a4172601e91205efb71823144a13e12c9ac
Mario Nikolov <marionikolov1@gmail.com>
2016-11-29 16:33:29 +0000

    [w8/1] implemented Dijkstra's algorithm to WeightedGraph class

--- Week 8/1. Weighted Graph Class with Dijkstra's Algorithm.py
+++ Week 8/1. Weighted Graph Class with Dijkstra's Algorithm.py
@@ -1,8 +1,9 @@
 from queue import Queue
+import math
 
-class Graph:
+class WeightedGraph:
     """
-    Instantiates an undirected unweighted graph. Call empty and then fill in using the addVertex and addEdge methods.
+    Instantiates an undirected weighted graph. Call empty and then fill in using the addVertex and addEdge methods.
     Example: graph = Graph()
              graph.addVertex(1)
              graph.addVertex(2)
@@ -64,6 +65,32 @@ class Graph:
         file.write(str(visited))
         file.close()
 
+    """
+    """
+    def Dijkstra(self, source, destination):
+        source = self.vertices[source] # get the actual Vertex() object
+        destination = self.vertices[destination]
+
+        source.tentativeWeight = 0 # the souce is at distance 0 from itself
+        v = source # currently scanned vertex
+        visited = []
+
+        while v != destination:
+            for u in v.adjacent.keys():
+                u = self.vertices[u] # see whether i can remove that
+                if v.tentativeWeight + v.adjacent[u.label] < u.tentativeWeight:
+                    u.tentativeWeight = v.tentativeWeight + v.adjacent[u.label]
+                    u.pre = v # store the return path
+
+            visited.append(v)
+            minimum = math.inf
+
+            for n in self.vertices.values():
+                if n not in visited and n.tentativeWeight < minimum:
+                    v = n
+                    minimum = n.tentativeWeight
+        return v.tentativeWeight
+
     def __str__(self):
         return str([str(x) for x in self.vertices.values()]) # uses list comprehension to print all the vertices
 
@@ -74,6 +101,8 @@ class Vertex:
     def __init__(self, label):
         self.label = label # the value of the vertex
         self.adjacent = {} # dictionary of all adjacent vertices; key: vertex label, value: weight of edge
+        self.tentativeWeight = math.inf # tentative weight used for Dijkstra's algorithm; default value is infinity
+        self.pre = None # return path used for Dijkstra's algorithm
 
     def addAdjacent(self, node, weight):
         self.adjacent[node] = weight
@@ -85,19 +114,31 @@ class Vertex:
         return str(self.label) + ": " + str([(x, k) for x,k in self.adjacent.items()]) # uses list comprehension to print all the adjacent vertices and the weight of the edges connceting them
 
 if __name__ == '__main__':
-    graph1 = Graph()
+    graph1 = WeightedGraph()
     graph1.addVertex(1)
     graph1.addVertex(2)
     graph1.addVertex(3)
     graph1.addVertex(4)
+    graph1.addVertex(5)
+    graph1.addVertex(6)
+    graph1.addVertex(7)
+    graph1.addVertex(8)
 
     graph1.addEdge(1, 2, 4)
     graph1.addEdge(1, 3, 6)
     graph1.addEdge(2, 3, 7)
     graph1.addEdge(3, 4, 3)
     graph1.addEdge(2, 4, 10)
+    graph1.addEdge(4, 5, 10)
+    graph1.addEdge(5, 6, 9)
+    graph1.addEdge(6, 7, 5)
+    graph1.addEdge(7, 8, 10)
+    graph1.addEdge(6, 8, 9)
 
     print(graph1)
 
     DFSresult = graph1.DFS(2)
     BFSresult = graph1.BFS(2)
+
+    res = graph1.Dijkstra(1,8)
+    print(res)
